f8938b31e95ae341e96fa226106a7942e9690aa5
JungSangHyeon <dev.jusa@gmail.com>
2021-11-23 18:08:45 +0900

    Add BottomNavigation

--- app/src/main/java/com/example/greenday/MainActivity.java
+++ app/src/main/java/com/example/greenday/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.greenday;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView menu = this.findViewById(R.id.menu);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+        NavController mainNavController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(menu, mainNavController);
     }
 }
